51a2ad638055cf0f2c3516dab985df07522f5fcf
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-17 16:07:11 +0700

    selecting_element_in_the_DOM

---
+++ app.js
@@ -0,0 +1,19 @@
+//GetElementById()
+
+const title = document.getElementById('main-heading');
+console.log(title)
+
+//GetElementByClassName()
+const listItems = document.getElementsByClassName('list-items');
+console.log(listItems)
+
+//getElementsByTagName()
+const list_items = document.getElementsByTagName('li');
+console.log(list_items)
+
+//querySelector()
+const container = document.querySelector('div');
+console.log(container);
+//querySelectorAll()
+const containers = document.querySelectorAll('div');
+console.log(containers);
