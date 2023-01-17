ee2215d1c14689464c91808a2f4fba7d3684b486
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-17 17:21:19 +0700

    modifying_element_attributes&classes

--- app.js
+++ app.js
@@ -21,5 +21,15 @@ const firstListItem = document.querySelector('.list-items');
 console.log(firstListItem.innerText);
 console.log(firstListItem.textContent);
 console.log(firstListItem.innerHTML)
-
 li.textContent = 'X-men'
+
+// Modifying attributes & classes
+//attributes
+// li.setAttribute('id', 'main-heading');
+// li.removeAttribute('id');
+// const title = document.querySelector('#main-heading');
+// console.log(title.getAttribute('id'))
+
+//classes
+li.classList.add('list-items');
+li.classList.remove('list-items');
