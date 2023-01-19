7eb64b60cd08da213cc9ff8bf424321b94b622b7
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-19 11:10:40 +0700

    event_listener

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-//Parent node traversal
-// let ul = document.querySelector('ul');
-// console.log(ul.parentNode);
-// console.log(ul.parentElement);
-// console.log(ul.parentNode.parentNode);
-// console.log(ul.parentElement.parentElement);
+//Event Listeners
+// const button = document.querySelector('.btn-2');
+// function alertBtn() {
+//     alert('I also love JS');
+// }
+// button.addEventListener("click", alertBtn)
 
-//let ul = document.querySelector('ul');
-// const html = document.documentElement;
-// console.log(html.parentNode);
-// console.log(html.parentElement);
-
-//Child node traversal
-//let ul = document.querySelector('ul');
-// console.log(ul.childNodes);
-// console.log(ul.firstChild);
-// console.log(ul.lastChild);
-// ul.childNodes[1].style.backgroundColor = 'blue'
-
-// let ul = document.querySelector('ul');
-// console.log(ul.firstElementChild);
-// console.log(ul.lastElementChild);
-
-//Sibling node traversal
-// let ul = document.querySelector('ul');
-// const div = document.querySelector('div');
-// console.log(div.childNodes);
-// console.log(ul.previousSibling);
-// console.log(ul.nextSibling);
-
-let ul = document.querySelector('ul');
-const div = document.querySelector('div');
-console.log(ul.previousElementSibling);
-console.log(ul.nextElementSibling)
+//Mouseover
+const newBackgroundColor = document.querySelector('.box-1');
+console.log(newBackgroundColor);    
+function changeBgColor() {
+    newBackgroundColor.style.backgroundColor = 'blue';
+}
+newBackgroundColor.addEventListener("mouseover", changeBgColor)
